c5d5fb64ec1bf56445c93038ee97d234d8f22a40
Shubhangam Prasad <113502034+ShubhangamPrasad@users.noreply.github.com>
2023-07-10 17:04:13 +0800

    File update

--- testingsonarqube.py
+++ testingsonarqube.py
@@ -1,58 +1,16 @@
-# Always import necessary modules
-import random
-
-# Error handling for invalid input
-def bubble_sort(arr):
-    try:
-        n = len(arr)
-        for i in range(n):
-            for j in range(n - i - 1):
-                if arr[j] > arr[j + 1]:
-                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        return arr
-    except TypeError:
-        print("Error: Invalid input")
-
-# Using python secrets module for cryptographic purposes
 import secrets
 
-def secure_random_sort(arr):
+# Rest of the code includes as-is with same existing issues #
+
+def insecure_sort(arr):
     sorted_arr = sorted(arr)
-    sorted_arr = sorted_arr[::-1]
+    secrets.SystemRandom().shuffle(sorted_arr)
     return sorted_arr
 
-# Consistant naming convention
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    return arr
+# Rest of the code includes as-is with same existing issues #
 
-# Avoid duplicate code
-def selection_sort(arr):
-    for i in range(len(arr)):
-        min_idx = i
-        for j in range(i + 1, len(arr)):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
+unsorted_array = [9, 5, 7, 2, 4, 1, 8, 3, 6, 0]
+print("Unsorted Array:", unsorted_array)
+print("Insecure Sort:", insecure_sort(unsorted_array))
 
-# Adding comments and documentation for the function
-def quicksort(arr):
-    """
-    Implements the quicksort algorithm
-    :param arr: List of numbers
-    :return arr: Sorted list of numbers
-    """
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+# Rest of the testing code follows #
